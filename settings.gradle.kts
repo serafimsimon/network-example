ff12f957fc38ec5e70eb7af9cfe783d0767b5022
Lezenford <alex.v.plekhanov@gmail.com>
2022-04-08 14:52:42 +0300

    update example

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,12 @@
 rootProject.name = "network"
-include(":io:server", ":io:client", ":nio:server", ":nio:client", ":netty:server", ":netty:client", ":netty:common")
+include(
+    ":io:server",
+    ":io:client",
+    ":nio:server",
+    ":nio:client",
+    ":netty:advanced:server",
+    ":netty:advanced:client",
+    ":netty:advanced:common",
+    ":netty:simple:server",
+    ":netty:simple:client"
+)
